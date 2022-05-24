e09369f8281b214a0218bf6f45ba59479505420a
Deivison Nogueira <deivison.nogueira@live.com>
2022-05-23 22:30:17 -0300

    alter

--- src/main/java/com/relacionamento/relacao/services/DesenvolvedoresService.java
+++ src/main/java/com/relacionamento/relacao/services/DesenvolvedoresService.java
@@ -1,5 +1,7 @@
 package com.relacionamento.relacao.services;
 
+import java.util.List;
+
 import com.relacionamento.relacao.model.Desenvolvedores;
 import com.relacionamento.relacao.repository.DesenvolvedorRepository;
 
@@ -7,24 +9,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DesenvolvedoresService {
-    
-     DesenvolvedorRepository repository;
 
-    public void buscaId(Desenvolvedores id ) {
-            repository.findAll(); 
+    DesenvolvedorRepository repository;
+
+    public List<Desenvolvedores> getDesenvolvedores() {
+        return repository.findAll();
     }
 
-    public void insereDev(Desenvolvedores desenvolvedores ){
-            repository.save(desenvolvedores);
-            
+    public void buscaId(Desenvolvedores id) {
+        repository.findAll();
     }
 
-    public void atualizaDev(Desenvolvedores desenvolvedores){
-         repository.save(desenvolvedores);
+    public void insereDev(Desenvolvedores desenvolvedores) {
+
+        this.repository.save(desenvolvedores);
 
     }
 
-    public void deleteDev(Desenvolvedores desenvolvedores){
-         repository.delete(desenvolvedores);
+    public void atualizaDev(Desenvolvedores desenvolvedores) {
+        repository.save(desenvolvedores);
+
+    }
+
+    public void deleteDev(Desenvolvedores desenvolvedores) {
+        repository.delete(desenvolvedores);
     }
 }
